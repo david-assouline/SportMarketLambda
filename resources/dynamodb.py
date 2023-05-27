45dd7bf7cddedd7d42f197579196cc6e66a416db
David <david.assouline@hotmail.com>
2023-05-27 16:22:25 -0400

    fixes

--- resources/dynamodb.py
+++ resources/dynamodb.py
@@ -4,17 +4,6 @@ import boto3
 
 AWS_DEFAULT_REGION = "us-east-2"
 
-
-# def create_ddb_instance():
-#     resource = boto3.resource(
-#         service_name="dynamodb",
-#         region_name=AWS_DEFAULT_REGION,
-#         aws_access_key_id='AKIA3PB7FSYDN2KZWNA2',
-#         aws_secret_access_key='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',
-#     )
-#     # ddb_exceptions = dynamodb_client.exceptions
-#     return resource
-
 def create_ddb_instance():
     sts_client = boto3.client('sts')
 
